cca6799fa6cf55a568d16595f97d24a0fadd2323
Manipi Harsha <manipiharsha@gmail.com>
2023-02-20 12:46:39 +0530

    "Adding the operations"

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
+// Multiplication
 /*
-       perform artithmatic operation here.
-       
-       your code goes here.
-*/
+var sum =function add (a, b) {
+       return a+b;
+   }
+   
+   const total =sum(3,5);
+   console.log(total);*/
+/*
+/Subtraction
+var sum =function sub (a, b) {
+       return a-b;
+   }
+   
+   const total =sum(3,5);
+   console.log(total); */
