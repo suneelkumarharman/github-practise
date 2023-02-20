1a5aba25030c0933987b96cb780994a20d7014a4
Manipi Harsha <manipiharsha@gmail.com>
2023-02-20 14:20:29 +0530

    Adding Arithmetic Operations

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
-/*
-       perform artithmatic operation here.
-       
-       your code goes here.
-*/
+// Multiplication
+
+var sum =function add (a, b) {
+       return a+b;
+   }
+   
+   const total =sum(3,5);
+   console.log(total);
+
+//Subtraction
+
+var sum = function sub (a, b) {
+       return a-b;
+   }
+   
+   const res =sum(3,5);
+   console.log(res); 
