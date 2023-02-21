474d18195965a1e9b7d52de6cfe03630488b85be
Ravi Kumar MR <ravikumarmreng2022@gmail.com>
2023-02-21 15:45:42 +0530

    Adding arthimetic operations

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
-/*
-       perform artithmatic operation here.
-       
-       your code goes here.
-*/
+// Addition
+
+const sum = function add(a, b) {
+    return a + b;
+}
+const total = sum(8, 9);
+
+
+// Division
+
+const sol = function divide(a, b) {
+    return a / b;
+}
+const res = sol(9, 3);
